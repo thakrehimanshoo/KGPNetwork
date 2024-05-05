02ef93befa63324c7d0929c6d031a5a244da4fe6
Himanshoo <thakrehimanshoo@gmail.com>
2024-05-05 23:37:51 +0530

    .02

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
-# Create your views here.
+
+def login(request):
+        if request.method == 'POST':
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('/')
+            else:  
+                return render(request, 'login.html', {'error': True})
+        return render(request, 'login.html')        
 def home(request):
+    if request.user.is_anonymous:
+        return redirect('/login/')
     return render(request, 'home.html')
+def logout(request):
+    auth_logout(request)
+    return redirect('/login/')
